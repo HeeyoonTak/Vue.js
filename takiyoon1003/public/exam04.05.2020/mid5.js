6e1f0930a224cb1d5cac117ec054724481860c79
Heeyoon <wewe1003@naver.com>
2020-05-04 10:00:21 +0900

    중간 커밋

--- takiyoon1003/public/exam04.05.2020/mid5.js
+++ takiyoon1003/public/exam04.05.2020/mid5.js
@@ -3,3 +3,13 @@ for(let i=0; i<100; i++){
     array[i] = Math.floor(Math.random() * (100 - 1 + 1) + 1);
 }
 
+let array2 = array.filter(function (n) {
+    return n % 2 == 0;
+});
+
+let sum = 0;
+for(let i = 0; i<array2.length; i++){
+    sum+=array2[i];
+}
+
+console.log(sum);
